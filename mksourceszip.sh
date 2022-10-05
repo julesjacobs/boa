a1e2b7e375af3b07d00f5c0e1192f8c7af0db780
julesjacobs <julesjacobs@gmail.com>
2022-10-06 01:00:02 +0200

    artifact

--- mksourceszip.sh
+++ mksourceszip.sh
@@ -1,5 +1,6 @@
 git archive HEAD -o sources.zip
 zip -d sources.zip misc
 zip -d sources.zip misc/*
-zip -d sources.zip mkmpgv.sh
+zip -d sources.zip mksourceszip.sh
 zip -d sources.zip benchresults.csv
+zip -d sources.zip sources.zip
