9c7b2313569382896a75d9b8ea2439f65e8f8106
julesjacobs <julesjacobs@gmail.com>
2022-10-24 14:46:42 +0200

    Benchmark results

--- src/main.rs
+++ src/main.rs
@@ -1078,6 +1078,27 @@ fn init_partition_ids_unsafe(data: &[u8], r: &CReader) -> Vec<u64> {
 fn count_parts(sigs: &[u64]) -> usize {
     let mut sigs2 = sigs.to_vec();
     sigs2.sort_unstable();
+    let mut freqs : HMap<u32,u32> = HMap::default();
+    let mut lastsig:u64 = 0;
+    let mut count = 0;
+    for &sig in &sigs2 {
+        if sig == lastsig {
+            count += 1;
+        }else{
+            if freqs.contains_key(&count){
+                freqs.insert(count, freqs[&count]+1);
+                // if count == 2 {
+                    // println!("Alarm: {:?} {:?} {:?} {:?}", sig, lastsig, count, freqs);
+                // }
+            }else{
+                freqs.insert(count, 1);
+            }
+            lastsig = sig;
+            count = 1;
+        }
+    }
+    freqs.remove(&0);
+    println!("Partition freqs: {:?}", std::collections::BTreeMap::from_iter(freqs.iter()));
     sigs2.dedup();
     return sigs2.len();
 }
